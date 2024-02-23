0ddaaa624317f1a5b62fa48d26fadc4bb08d7f33
ella <111292475+Dev-ellaij@users.noreply.github.com>
2024-02-23 23:11:14 +0100

    Add files via upload

---
+++ app_export.dart
@@ -0,0 +1,14 @@
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+export 'package:davesbirthdayapp/core/utils/image_constant.dart';
+export 'package:davesbirthdayapp/core/utils/navigator_service.dart';
+export 'package:davesbirthdayapp/core/utils/pref_utils.dart';
+export 'package:davesbirthdayapp/core/utils/size_utils.dart';
+export 'package:davesbirthdayapp/data/models/selectionPopupModel/selection_popup_model.dart';
+export 'package:davesbirthdayapp/localization/app_localization.dart';
+export 'package:davesbirthdayapp/routes/app_routes.dart';
+export 'package:davesbirthdayapp/theme/app_decoration.dart';
+export 'package:davesbirthdayapp/theme/custom_text_style.dart';
+export 'package:davesbirthdayapp/theme/theme_helper.dart';
+export 'package:davesbirthdayapp/widgets/custom_image_view.dart';
+export '../theme/notifier/theme_notifier.dart';
+export 'package:davesbirthdayapp/core/utils/date_time_utils.dart';
---
+++ image_constant.dart
@@ -0,0 +1,22 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // iPhone 13 & 14 - One images
+  static String imgCashPaymentBag16 = '$imagePath/img_cash_payment_bag_1_6.png';
+
+  static String imgEllipse2 = '$imagePath/img_ellipse_2.png';
+
+  static String imgVideoGameControllerWifi16 =
+      '$imagePath/img_video_game_controller_wifi_16.png';
+
+  static String imgWirelessHouseSignal28 =
+      '$imagePath/img_wireless_house_signal_28.png';
+
+  static String imgSmileyKissHeart20 =
+      '$imagePath/img_smiley_kiss_heart_20.png';
+
+  static String imgBusinessManage = '$imagePath/img_business_manage.png';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+}
